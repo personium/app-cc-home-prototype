5cdf92721a3ed3e2beb64db72184cb2d8f0c1fb1
Unuma-Fuyuka <unuma.fuyuka@jp.fujitsu.com>
2018-03-14 14:29:01 +0900

    ADD:account.html, links.html, profile.html

--- src/html/js/homeapp.js
+++ src/html/js/homeapp.js
@@ -19,8 +19,8 @@
  * The followings should be shared among applications.
  */
 
- /* Application & Service Padding Function*/
 $(function () {
+    /*-------------------------main page(From here)-------------------------*/
     var timer = false;
     $(window).on('resize', function () {
         if (timer !== false) {
@@ -33,4 +33,104 @@ $(function () {
             $('.app-list').css('padding-left', padding);
         }, 50);
     }).resize();
-});
+    /*-------------------------main page(To here)-------------------------*/
+
+    /*-------------------------profile page(From here)-------------------------*/
+    /*Edit button clicked action*/
+    $('.edit-btn').on('click', function () {
+
+        /*User Name Edit Button Clicked*/
+        if ($(this).attr('id') == 'user-name-edit-btn') {
+            if (!($(this).hasClass('editing'))) {
+                if (($(this).hasClass('edited'))) {
+                    $(this).removeClass('edited');
+                }
+                $(this).addClass('editing');
+                $('#user-name-form').focus();
+            } else if (($(this).hasClass('editing')) && !($(this).hasClass('edited'))) {
+                $(this).removeClass('editing');
+                $(this).addClass('edited');
+                var edit_ic = $(this).find('.fa-edit');
+                edit_ic.removeClass('fa-edit');
+                edit_ic.addClass('fa-check');
+            }
+
+            /*Description Edit Button Clicked*/
+        } else if ($(this).attr('id') == 'description-edit-btn') {
+            if (!($(this).hasClass('editing')) && !($(this).hasClass('edited'))) {
+                $(this).addClass('editing');
+                $('.description-text').css('display', 'none');
+                $('#description-form').css('display', 'block');
+                $('#description-form-area').focus();
+            } else if (($(this).hasClass('editing')) && !($(this).hasClass('edited'))) {
+                $(this).removeClass('editing');
+                $(this).addClass('edited');
+                $('#description-form').css('display', 'none');
+                $('.description-text').css('display', 'block');
+                var edit_ic = $(this).find('.fa-edit');
+                edit_ic.removeClass('fa-edit');
+                edit_ic.addClass('fa-check');
+            }
+        }
+    })
+    /*-------------------------profile page(To here)-------------------------*/
+
+    /*-------------------------account page(From here)-------------------------*/
+    /*Edit Button Clicked(Account Page's Header)*/
+    $('#account_mng_edit_btn').on('click', function () {
+        if (!($(this).hasClass('editing'))) {
+            if (($(this).hasClass('edited'))) {
+                $(this).removeClass('edited');
+            }
+            $(this).addClass('editing');
+            $('.account_mng_list>li').filter(":last").css('display', 'none');
+            $('.account-name').addClass('edit-ic');
+            $('.account_mng_list > li > div').animate({
+                'left': '0px'
+            }, 500);
+        } else if (($(this).hasClass('editing')) && !($(this).hasClass('edited'))) {
+            $(this).removeClass('editing');
+            $(this).addClass('edited');
+            $('.account_mng_list > li > div').animate({
+                'left': '-70px'
+            }, 500);
+            $('.account_mng_list>li').filter(":last").css('display', 'block');
+            $('.account-name').removeClass('edit-ic');
+            $('.account-name').removeClass('clear-ic');
+        }
+    })
+
+    /*Circle Delete Button Clicked(Account Page's List Left)*/
+    $('.delete-check-btn').on('click', function () {
+        $(this).parent().animate({
+            'left': '-170px'
+        }, 500);
+        $(this).next().removeClass('enpitsu');
+        $(this).next().addClass('clear-ic');
+    })
+
+    /*Square Delete Button Clicked(Account Page's List Right)*/
+    $('.account-delete-btn').on('click', function () {
+        $(this).closest('li').animate({
+            width: 'hide',
+            height: 'hide',
+            opacity: 'hide'
+        }, 'slow', function () {
+            $(this).remove();
+        });
+    });
+
+    /*Deletion When clicking an element being checked*/
+    $(".account-name").on("click", function () {
+        if ($(this).hasClass('clear-ic')) {
+            if ($('#account_mng_edit_btn').hasClass('editing')) {
+                $('.account_mng_list > li > div').animate({
+                    'left': '0px'
+                }, 500);
+                $(this).removeClass('clear-ic');
+            }
+
+        }
+    });
+    /*-------------------------account page(To here)-------------------------*/
+})
