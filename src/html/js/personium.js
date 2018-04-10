b5baa3766254ee2d78f36422dfd689d8cc028122
unuma fuyuka <unuma.fuyuka@jp.fujitsu.com>
2018-04-10 13:50:26 +0900

    UPDATE:scroll area

--- src/html/js/personium.js
+++ src/html/js/personium.js
@@ -21,23 +21,23 @@
 
 
 $(function () {
-  function init() {
-    // スクロールして何ピクセルでアニメーションさせるか
-    var px_change   = 300;
+//   function init() {
+//     // スクロールして何ピクセルでアニメーションさせるか
+//     var px_change   = 300;
  
-    // スクロールのイベントハンドラを登録
-    window.addEventListener('scroll', function(e){
-        // 変化するポイントまでスクロールしたらクラスを追加
-        if ( $(window).scrollTop() > px_change ) {
-            $("header").addClass("smaller");
+//     // スクロールのイベントハンドラを登録
+//     window.addEventListener('scroll', function(e){
+//         // 変化するポイントまでスクロールしたらクラスを追加
+//         if ( $(window).scrollTop() > px_change ) {
+//             $("header").addClass("smaller");
  
-        // 変化するポイント以前であればクラスを削除
-        } else if ( $("header").hasClass("smaller") ) {
-            $("header").removeClass("smaller");
-        }
-    });
-}
-window.onload = init();
+//         // 変化するポイント以前であればクラスを削除
+//         } else if ( $("header").hasClass("smaller") ) {
+//             $("header").removeClass("smaller");
+//         }
+//     });
+// }
+// window.onload = init();
 
 
 
